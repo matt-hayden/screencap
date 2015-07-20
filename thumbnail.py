afca9ea2b76098b85a296e42766642304bf9af00
Matt Hayden <hayden+github@morp.holo.gy>
2015-07-19 18:30:01 -0600

    Refined for CLI

--- thumbnail.py
+++ thumbnail.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import os, os.path
+import tempfile
 
 from FFMpeg import *
 from ImageMagick import montage
 
-def thumbnail(input_filename, count=56, **kwinput_filenames):
+def thumbnail(input_filename, output_filename=None, count=56, **kwargs):
 	dirname, basename = os.path.split(input_filename)
-	title, _ = os.path.splitext(basename)
-	fs = list(thumbnails(input_filename))
+	filepart, _ = os.path.splitext(basename)
+	size = os.path.getsize(input_filename)
+	if not output_filename:
+		output_filename = os.path.join(dirname, "{}-{}-screens.JPG".format(filepart, size))
+	title = "{} - {:,} B".format(basename, size)
+	fs = list(thumbnails(input_filename, output_file_pattern=os.path.join(tempfile.mkdtemp(), '%08d.PNG')) )
 	n = len(fs)
 	if n < count:
 		selection = fs
@@ -15,11 +20,10 @@ def thumbnail(input_filename, count=56, **kwinput_filenames):
 		selection = fs[::n//count]
 		while count < len(selection):
 			selection.pop(len(selection)//2)
-	montage(selection, output_file=basename+'_montage'+'.jpg', title=title, **kwinput_filenames)
+	montage(selection, output_filename=output_filename, title=title, **kwargs)
 if __name__ == '__main__':
 	import sys
-	myoptions = { 'tile': '8x', 'geometry': '+0+0', 'border': 1,
-				  'background': 'transparent', 'bordercolor': 'transparent', 'fill': 'white', 'stroke': 'gray' }
+	myoptions = { 'tile': '8x', 'geometry': '+0+0', 'border': 1, 'quality': '35' }
 	for arg in sys.argv[1:]:
 		print("Generating thumbnails for", arg)
 		thumbnail(arg, **myoptions)
