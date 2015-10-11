923e69852f193bb241f6743acedd9caf0a5716b1
Matt Hayden <hayden+github@morp.holo.gy>
2015-10-10 20:56:20 -0600

    Initial commit

---
+++ scripts/blackdetect.bash
@@ -0,0 +1,105 @@
+#! /bin/bash
+: ${FFMPEG=ffmpeg} ${FFPROBE=ffprobe}
+set -e
+
+SCRIPT=$(basename ${BASH_SOURCE[0]})
+
+: ${log=`mktemp`} ${errors=`mktemp`}
+
+
+function help() {
+	cat <<- EOF
+This is help
+EOF
+	caller 0
+	exit -1
+}
+
+function usage() {
+	cat <<- EOF
+This is usage
+EOF
+	caller 0
+	exit -1
+}
+
+function die() {
+	echo <<< "$@"
+	exit -1
+}
+
+
+while getopts ":hfno:qv0123456789 -:" OPT
+do
+	if [[ $OPT == '-' ]] # Long option
+	then
+		OPT=$OPTARG
+		eval $OPT && continue || usage # you may or may not want the continue
+	fi
+	case $OPT in
+		-) # long argument, ignore ${OPTARG}
+		;;
+		h|help) help
+		;;
+		f|overwrite) overwrite=1
+		;;
+		o|output) out="$OPTARG"
+		;;
+		q|quiet) quiet=1
+		;;
+		[0-9]) mode="$OPTARG"
+		;;
+		\?) usage # unrecognized options would cause bash error
+		;;
+	esac
+done >&2
+shift $((OPTIND-1))
+
+output_options="-show_entries tags=lavfi.black_start,lavfi.black_end,lavfi.scene_score -of flat"
+case mode in
+	0)
+		function blackdetect() {
+			file_in="$1"
+			$FFPROBE -f lavfi "movie=${file_in},blackdetect=[out0]" $output_options
+		}
+	;;
+	1|2|3)
+		function blackdetect() {
+			file_in="$1"
+			$FFPROBE -f lavfi "movie=${file_in},blackdetect=d=1[out0]" $output_options
+		}
+	;;
+	4|5|6)
+		function blackdetect() {
+			file_in="$1"
+			$FFPROBE -f lavfi "movie=${file_in},blackdetect=d=1/15[out0]" $output_options
+		}
+	;;
+	7|8|9)
+		function blackdetect() {
+			file_in="$1"
+			$FFPROBE -f lavfi "movie=${file_in},blackdetect=d=1/30:picture_black_ratio_th=0.75:pixel_black_th=0.50[out0]" $output_options
+		}
+	;;
+esac
+
+file_in="$1"
+shift
+[[ $out ]] || out="${file_in##*/}.blackdetect"
+[[ -e "$out" ]] && ! $((overwrite)) || die "Refusing to overwrite $out"
+[[ "$@" ]] && usage
+
+if blackdetect "$file_in" >"$log" 2>"$errors"
+then
+	if [[ -s "$log" ]]
+	then
+		if mv -b "$log" "$out"
+		then
+			$((quiet)) || echo "$file_in => $out"
+		fi
+		exit 0
+	fi
+else
+	cat "$errors" >&2
+fi
+exit 1
---
+++ scripts/thumbs.bash
@@ -0,0 +1,107 @@
+#! /bin/bash
+: ${FFMPEG=ffmpeg} ${MPLAYER=mplayer}
+
+set -e
+
+SCRIPT=$(basename ${BASH_SOURCE[0]})
+
+function help() {
+	cat <<- EOF
+This is help
+EOF
+	caller 0
+	exit -1
+}
+
+function usage() {
+	cat <<- EOF
+This is usage
+EOF
+	caller 0
+	exit -1
+}
+
+function die() {
+	echo <<< "$@"
+	exit -1
+}
+
+mode=1
+while getopts ":hd:f:0123456789 -:" OPT
+do
+	if [[ $OPT == '-' ]] # Long option
+	then
+		OPT=$OPTARG
+		eval $OPT && continue || usage # you may or may not want the continue
+	fi
+	case $OPT in
+		-) echo Long option: $OPT $OPTARG # the continue above prevents reaching this
+		;;
+		h|help) help
+		;;
+		d|dest) dest="$OPTARG"
+		;;
+		f|format) format="$OPTARG"
+		;;
+		[0-9]) mode=$OPT
+		;;
+		\?) usage # getopts replaces unknown with ?
+		;;
+	esac
+done >&2
+shift $((OPTIND-1))
+
+## defaults
+format=png # jpeg
+##
+
+ext="${format##.}"
+
+file_in="$1"
+[[ -f "$file_in" ]] || die "file $file_in not found"
+shift
+[[ "$@" ]] && usage
+basename="${file_in##*/}"
+filepart="${basename%.*}"
+[[ $dest ]] || dest="${basename}_thumbs"
+mkdir -p "$dest"
+[[ $errors ]] || errors="$dest/errors"
+
+case mode in
+	0|1|2)
+		function thumbs() {
+			file_in="$1"
+			out="${dest}/${filepart}_%08d${ext}"
+			$FFMPEG -an -nostdin -i "$file_in" -f image2 -vframes 1 -vsync drop "$out"
+		}
+	;;
+	3|4)
+		function thumbs() {
+			file_in="$1"
+			out="${dest}/${filepart}_%08d${ext}"
+			$FFMPEG -an -nostdin -i "$file_in" -f image2 -vf 'fps=1/240' -vsync drop "$out"
+		}
+	;;
+	5|6|7)
+		function thumbs() {
+			file_in="$1"
+			out="${dest}/${filepart}_%08d${ext}"
+			$FFMPEG -an -nostdin -i "$file_in" -f image2 -vf select='eq(pict_type\,PICT_TYPE_I)' -vsync drop "$out"
+		}
+	;;
+	8|9)
+		function thumbs() {
+			file_in="$1"
+			out="${format}:${dest}"
+			$MPLAYER -nosound -ss 240 "$file_in" -frames 64 -vo "$out"
+		}
+	;;
+esac
+
+if thumbs "$file_in" &> "$errors"
+then
+	exit 0
+else
+	cat "$errors"
+fi >&2
+exit 1
