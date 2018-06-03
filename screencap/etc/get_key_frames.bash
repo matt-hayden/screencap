0e517f64ed9f45dcb12f2c22417d3df2cea3f0a3
Matt Hayden (Valenceo LTD.) <https://github.com/matt-hayden>
2018-06-03 11:39:27 -0600

    Use new insert_screencap_defaults command

--- screencap/etc/get_key_frames.bash
+++ screencap/etc/get_key_frames.bash
@@ -1,50 +1,27 @@
 #! /usr/bin/env bash 
 set -e
-: ${FFPROBE=ffprobe} ${PV=pv}
+: ${FFPROBE=ffprobe} ${PV=pv -l -N frames}
 
+type ionice &> /dev/null && FFPROBE="ionice -n 7 $FFPROBE"
 [[ -t 2 ]] && FFPROBE="${FFPROBE} -loglevel warning"
 
-function get_nframes() {
-  $FFPROBE -select_streams v -show_streams -print_format json "$@" |
-  jq '[ .streams | .[].nb_frames | tonumber ] | max'
-}
-
-while getopts ":n:q" flag
-do
-  case "$flag" in
-    n) nframes="$OPTARG" ;;
-    q) quiet=1 ;;
-    :)
-      echo "Option -$OPTARG requires an argument."
-      exit 1
-      ;;
-    \?) # Error condition is literal '?'
-      cat <<-EOF
-Usage: get_key_frames
-  -n nframes  Override automatic detection of number of frames
-  -q    No output
-
-EOF
-      exit 1
-      ;;
-    *) # Should not occur
-      echo "Unknown error while processing options"
-      ;;
-  esac
-done >&2
-shift $((OPTIND-1))
-
-(( quiet )) && FFPROBE="${FFPROBE} -loglevel error"
 
 mkdir -p "$HOME/.cache/key_frames"
 input_path="$@"
 [[ -s "$input_path" ]] || echo "'$input_path' not found!" >&2
-if [[ $nframes ]] || nframes=$(get_nframes "$input_path")
+if [[ -t 0 ]]
 then
-  PV="${PV} -s $nframes"
+  json_info=$($FFPROBE -select_streams v -show_streams -print_format json "$input_path")
+else
+  json_info=$(jq -c .) # capture stdin
 fi
+nframes=$(jq '[ .streams[] | select(.codec_type=="video") | .nb_frames | tonumber ] | max' <<< "$json_info") && PV="${PV} -s $nframes"
+
 filename="${input_path##*/}"
-for outfile in "${input_path}.key_frames" "${filename}.key_frames" "$HOME/.cache/key_frames/${nframes}-${filename}.key_frames"
+for outfile in \
+	"${input_path}.key_frames" \
+	"${filename}.key_frames" \
+	"$HOME/.cache/key_frames/${nframes}-${filename}.key_frames"
 do
   [[ -s "$outfile" ]] && break
 done
@@ -53,7 +30,12 @@ then
   $FFPROBE -select_streams v -show_frames \
     -show_entries frame=key_frame,best_effort_timestamp_time \
     -print_format csv "$input_path" |
-  $PV -l -N "video frames" |
+  $PV |
   awk -F, 'BEGIN { nframes=0 } ($1=="frame") { nframes++ } $2 { print nframes, $3 }' > "$outfile"
 fi
-[[ -s "$outfile" ]] && echo "$outfile" || echo "'$input_path' failed!" >&2
+if [[ -s "$outfile" ]]
+then
+	echo "$outfile"
+else
+	echo "'$input_path' failed!" >&2
+fi
--- screencap/etc/screencap.bash
+++ screencap/etc/screencap.bash
@@ -11,12 +11,14 @@
 # $ screencap file < json
 # Annotations are extracted from json format
 #
-# $ screencap [-i file and other ffmpeg options]
-# This allows fine-tuning ffmpeg parameters, but no annotations
+# $ screencap file [ffmpeg options]
+# This allows fine-tuning ffmpeg parameters
 #
-# $ screencap [-i file and other ffmpeg options] < json
-# Most flexible and best option
-#
+# Variables observed:
+#  duration			Override detection of video duration
+#  edit_from		Specify a file to hold JSON probe results and edit it
+#  output			Image file to write
+#  seconds_between	Number of seconds between screen captures
 
 : ${FFMPEG=ffmpeg -nostdin} ${FFPROBE=ffprobe}
 set -e
@@ -24,70 +26,80 @@ export TMPDIR="$(mktemp -d -t $$.XXXXXXXX)"
 intermediate="$(mktemp -t XXXXXXXX.png)"
 
 type ionice &> /dev/null && FFMPEG="ionice -n 7 $FFMPEG"
+type ionice &> /dev/null && FFPROBE="ionice -n 7 $FFPROBE"
 
-if [[ -t 2 ]]
-then
+if [[ -t 2 ]]; then
   FFMPEG="${FFMPEG} -loglevel warning"
   FFPROBE="${FFPROBE} -loglevel warning"
 fi
 
 declare -a convert_args ffmpeg_args
-if [[ $# -eq 1 ]]
-then
+
+# defaults
+# This ffmpeg isn't happy doing 4K video tiled beyond n=30
+layout=3x10
+while getopts "ei:s:" flag
+do
+  case "$flag" in
+    e) edit_from="$(mktemp -t XXXXXXXX.json)" ;;
+	i) ;; # pass through for ffmpeg compatibility
+    s) layout="$OPTARG" ;;
+    :)
+      echo "Usage: -$flag requires an argument"
+      exit 10
+      ;;
+    #\?) # literal '?'
+    #*) # Should not occur
+  esac
+done >&2
+shift $((OPTIND-1))
+
+ffmpeg_args+=( -i )
+if [[ $# -eq 1 ]]; then
   input_path="$@"
   [[ -f "$input_path" ]]
-  ffmpeg_args+=( -i )
   ffmpeg_args+=( "${input_path}" )
 else
-  ffmpeg_args=( "$@" )
+  ffmpeg_args+=( "$@" )
 fi
 
-if [[ -t 0 ]]
-then
-  [[ $input_path ]] && json_info=$($FFPROBE -show_format -show_streams -show_chapters -print_format json "$input_path")
+
+if [[ -t 0 ]]; then
+  [[ -s $input_path ]] && probe_json=$($FFPROBE -show_format -show_streams -show_chapters -print_format json "$input_path")
 else
-  json_info=$(cat)
-  input_path=$(jq -r ".format.filename?" <<< "$json_info")
+  probe_json=$(jq -c .) # capture stdin and check it
+  input_path=$(jq -r ".format.filename?" <<< "$probe_json")
 fi
 
-if [[ $json_info ]]
-then
-  if ! [[ $title ]]
-  then
-    if jq -e ".format.tags?.title?" <<< "$json_info"
-    then
-    title=$(jq -r .format.tags.title <<< "$json_info")
-    else
-    title="${input_path##*/}"
-    fi > /dev/null
+if [[ $edit_from ]]; then
+  if ! [[ -s "$edit_from" ]]; then
+    insert_screencap_defaults <<< "$probe_json" > "$edit_from"
   fi
-  bit_rate=$(jq ".format.bit_rate | tonumber | . / 1E6" <<< "$json_info")
-  [[ $duration ]] || duration=$(jq ".format.duration? | tonumber" <<< "$json_info")
-  megapixels=$(jq '.streams[] | select(.codec_type == "video") | .width * .height / 1E6' <<< "$json_info")
-  file_size=$(jq '.format.size | tonumber' <<< "$json_info")
-
-  [[ $title ]] &&
-    convert_args+=( -gravity northwest -annotate +0+0 "$title" )
-  [[ $megapixels ]] && [[ $bit_rate ]] && 
-    convert_args+=( -gravity northeast -annotate +0+0 "$megapixels Mpx @ $bit_rate Mbit" )
-  duration_t=$(jq -r ".format.duration_t?" <<< "$json_info")
-  [[ $duration_t ]] || duration_t="$duration s"
-  [[ $duration_t ]] &&
-    convert_args+=( -gravity southwest -annotate +0+0 "$duration_t" )
-  file_size_t=$(jq -r ".format.size_t?" <<< "$json_info")
-  [[ $file_size_t ]] || file_size_t="$file_size bytes"
-  [[ $file_size_t ]] &&
-    convert_args+=( -gravity southeast -annotate +0+0 "$file_size_t" )
+  $VISUAL "$edit_from"
+  json_info=$(jq -c . < "$edit_from")
+else
+  json_info=$(insert_screencap_defaults <<< "$probe_json")
 fi
 
-[[ $output ]] || output="${title}.jpeg"
-# This ffmpeg isn't happy doing 4K video tiled beyond n=30
-if ! [[ $seconds_between ]]
-then
+[[ $output ]] || output="${input_path##*/}.jpeg"
+# duration is needed to calculate interval between screen captures
+[[ $duration ]] || duration=$(jq ".format.duration? | tonumber" <<< "$json_info")
+
+title=$(jq -r '.title? // .format.tags?.title?' <<< "$json_info") && title=$(basename "$title")
+[[ $title != null ]] &&
+  convert_args+=( -gravity northwest -annotate +0+0 "$title" )
+quality_label=$(jq -r '.quality_label?' <<< "$json_info") &&
+  convert_args+=( -gravity northeast -annotate +0+0 "$quality_label" )
+duration_label=$(jq -r ".duration_label?" <<< "$json_info") &&
+  convert_args+=( -gravity southwest -annotate +0+0 "$duration_label" )
+file_size_label=$(jq -r ".size_label?" <<< "$json_info") &&
+  convert_args+=( -gravity southeast -annotate +0+0 "$file_size_label" )
+
+if ! [[ $seconds_between ]]; then
   [[ $duration ]] && seconds_between=$(awk "BEGIN {print ${duration-900} / 31}")
 fi
 if $FFMPEG -skip_frame nokey -an -vsync 0 -y "${ffmpeg_args[@]}" \
-  -vf "select='isnan(prev_selected_t)+gte(t-prev_selected_t\\,${seconds_between-30})',tile=3x10" \
+  -vf "select='isnan(prev_selected_t)+gte(t-prev_selected_t\\,${seconds_between-30})',tile=${layout}" \
   -frames:v 1 \
   "$intermediate"
 then
@@ -98,4 +110,8 @@ then
     -fill gray95 -undercolor '#00000080' \
     -font Palatino-Bold -pointsize 28 -antialias \
     "${convert_args[@]}" "$output"
+  if type wrjpgcom &> /dev/null; then
+    wrjpgcom -comment "$json_info" < "$output" > t
+	[[ -s t ]] && mv t "$output"
+  fi
 fi
